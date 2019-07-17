eac31999812895ec987c10fcc66f2b461f9373f9
yanlele <331393627@qq.com>
2019-07-17 23:40:23 +0800

    [+]add create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,18 +2,26 @@
  * create by yanle
  * create time 2019/7/13 5:16 PM
  */
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import {UserService} from './user.service';
+import {CreateUserDto} from './dto';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {
+  }
+
+  @Post()
+  async create(@Body('user') userData: CreateUserDto) {
+    return this.userService.create(userData);
+  }
 
   @Get('test')
   test() {
     return {
       message: 'just test',
       success: true,
-      data: {}
-    }
+      data: {},
+    };
   }
 }
-
