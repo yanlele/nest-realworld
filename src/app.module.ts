2aca54b3233e1a9b47326c147a041ae970ebfa46
yanlele <331393627@qq.com>
2019-07-17 23:36:24 +0800

    [*]fix tslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import {Module} from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {UserModule} from "./user/user.module";
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UserModule} from './user/user.module';
 import {Connection} from 'typeorm';
 
 @Module({
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,14 @@
  * create time 2019/7/13 5:15 PM
  */
 import {Module} from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {UserEntity} from "./user.entity";
-import {UserController} from "./user.controller";
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UserEntity} from './user.entity';
+import {UserController} from './user.controller';
+import {UserService} from "./user.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [],
+  providers: [UserService],
   controllers: [UserController],
 })
 export class UserModule {
