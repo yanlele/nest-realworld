45cf97674a40ba7f47ab96dfe0748f1202b408c0
yanlele <331393627@qq.com>
2019-07-21 23:35:35 +0800

    [+]modify user info

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,11 +2,12 @@
  * create by yanle
  * create time 2019/7/13 5:16 PM
  */
-import {Body, Controller, Get, Post, Query} from '@nestjs/common';
+import {Body, Controller, Get, Post, Put, Query} from '@nestjs/common';
 import {UserService} from './user.service';
-import {CreateUserDto} from './dto';
+import {CreateUserDto, UpdateUserDto} from './dto';
 import {responseSuccessData, StandResponse} from '../utils/standResponse';
 import {UserRo} from './user.interface';
+import {UserEntity} from './user.entity';
 
 @Controller('user')
 export class UserController {
@@ -16,6 +17,11 @@ export class UserController {
   @Get()
   async findMe(@Query('email') email: string): Promise<StandResponse<UserRo>> {
     return responseSuccessData(await this.userService.findByEmail(email));
+  }
+
+  @Put()
+  async update(@Body('id') userId: number, @Body('user') userData: UpdateUserDto): Promise<StandResponse<UserEntity>> {
+    return responseSuccessData(await this.userService.update(userId, userData));
   }
 
   @Post()
